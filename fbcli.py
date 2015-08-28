632d1bce5155119ffd4ff4557c2f9f20ed56619b
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2015-08-28 08:14:02 +0100

    Initial commit

---
+++ fbcli.py
@@ -0,0 +1,465 @@
+#!/usr/bin/env python
+# pylint: disable=W0603
+
+# TODO
+# http://help.fogcreek.com/8202/xml-api
+# - edit
+# - see parent/see-also tickets
+# - bug events without a body
+# - package all up and pypi
+# - when closing, assign to opener
+
+from functools import wraps
+import getpass
+import logging
+import os
+import sys
+
+from fogbugz import FogBugz
+from tornado.template import Template
+
+import ui
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('fb')
+
+FBURL = os.environ.get('FBURL', 'https://yougov.fogbugz.com/')
+FB = FogBugz(FBURL)
+CURRENT_CASE = None
+CURRENT_USER = None
+
+COMMANDS = {}
+
+
+def set_current_case(case):
+    global CURRENT_CASE
+    CURRENT_CASE = case
+    return case
+
+
+def set_current_user(user):
+    global CURRENT_USER
+    CURRENT_USER = user
+    return user
+
+
+def command(*names):
+
+    def wrapper(f):
+
+        for name in names:
+            COMMANDS[name] = Command(f)
+
+        @wraps(f)
+        def helper(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        return helper
+
+    return wrapper
+
+
+class Command(object):
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        return self.f(*args, **kwargs)
+
+    def desc(self):
+        if self.f.__doc__ is None:
+            return '?'
+        return self.f.__doc__.splitlines()[0]
+
+    def help(self):
+        return self.f.__doc__
+
+
+class FBObj(object):
+
+    TMPL = None
+
+    def to_string(self):
+        return self.TMPL.generate(
+            obj=self,
+            ui=ui,
+        )
+
+    def __str__(self):
+        return self.to_string()
+
+
+class FBPerson(FBObj):
+
+    TMPL = Template('''{% raw obj.fullname %} <{% raw obj.email %}>''')
+
+    def __init__(self, person):
+        self._person = person
+
+    @classmethod
+    def get(cls, email):
+        persons = FB.viewPerson(sEmail=email)
+        person = persons.find('person')
+        return cls(person)
+
+    @property
+    def id(self):
+        return int(self._person.ixperson.text)
+
+    @property
+    def fullname(self):
+        return self._person.sfullname.text
+
+    @property
+    def email(self):
+        return self._person.semail.text
+
+
+class FBCase(FBObj):
+
+    TMPL = Template('''
+{{ ui.hl1 }}
+[{% raw ui.cyan(obj.id) %}] {% raw ui.blue(obj.title) %}
+{% raw obj.permalink %}
+{% raw ui.status(obj.status) %} - Assigned to {% raw ui.red(obj.assigned_to) %}
+{{ ui.hl1}}
+{% for event in obj.events %}
+{{ ui.hl2 }}
+{% raw event %}
+{% end %}
+''')
+
+    def __init__(self, ixBug):
+        self.id = int(ixBug)
+        self.reset()
+
+    def reset(self):
+        self._case = self._get_raw(self.id)
+        return set_current_case(self)
+
+    @staticmethod
+    def _get_raw(ixBug):
+        assert isinstance(ixBug, int)
+        cols = [
+            'sTitle',
+            'sStatus',
+            'sPersonAssignedTo',
+            'events',
+        ]
+        return FB.search(q=ixBug, cols=','.join(cols))
+
+    @property
+    def title(self):
+        return self._case.stitle.text
+
+    @property
+    def status(self):
+        return self._case.sstatus.text
+
+    @property
+    def assigned_to(self):
+        return self._case.spersonassignedto.text
+
+    @property
+    def events(self):
+        return [FBBugEvent(event) for event in self._case.events]
+
+    @property
+    def permalink(self):
+        return FBURL + 'f/cases/{}'.format(self.id)
+
+    @property
+    def shortdesc(self):
+        return '{} - {}'.format(
+            self.permalink,
+            self.title)
+
+    def resolve(self):
+        FB.resolve(ixBug=self.id, ixPersonEditedBy=CURRENT_USER.id)
+        self.reset()
+
+    def reopen(self):
+        FB.reopen(ixBug=self.id, ixPersonEditedBy=CURRENT_USER.id)
+        self.reset()
+
+    def close(self):
+        FB.close(ixBug=self.id, ixPersonEditedBy=CURRENT_USER.id)
+        self.reset()
+
+    def reactivate(self):
+        FB.reactivate(ixBug=self.id, ixPersonEditedBy=CURRENT_USER.id)
+        self.reset()
+
+    def assign(self, person):
+        FB.assign(
+            ixBug=self.id, ixPersonEditedBy=CURRENT_USER.id,
+            sPersonAssignedTo=person)
+        self.reset()
+
+    # def edit(self):
+    # def email(self):
+    # def remind(self):
+
+
+class FBBugEvent(FBObj):
+
+    TMPL = Template(
+        '''{{ obj.dt }} - {{ obj.person }}
+{% raw obj.comment %}''')
+
+    def __init__(self, event):
+        self._event = event
+
+    @property
+    def id(self):
+        return int(self._event.ixbugevent.text)
+
+    @property
+    def dt(self):
+        return self._event.dt.text
+
+    @property
+    def person(self):
+        return self._event.sperson.text
+
+    @property
+    def comment(self):
+        return self._event.s.text
+
+
+class FBShortCase(FBObj):
+
+    TMPL = Template(
+        '''{% raw ui.cyan(str(obj.id).rjust(6)) %} - \
+{% raw ui.status(ui.ltrunc(obj.status, 20)) %} {% raw ui.blue(obj.title) %}''')
+
+    def __init__(self, ixBug, status, title):
+        self.id = ixBug
+        self.status = status
+        self.title = title
+
+
+class FBCaseSearch(FBObj):
+
+    TMPL = Template('''
+{% for case in obj.shortcases %}{% raw case %}
+{% end %}''')
+
+    def __init__(self, shortcases):
+        self.shortcases = shortcases
+
+    @classmethod
+    def search(cls, q):
+        logger.info('Searching for %r', q)
+        cases = []
+        resp = FB.search(q=q, cols="ixBug,sTitle,sStatus")
+        for case in resp.cases.findAll('case'):
+            cases.append(FBShortCase(
+                int(case.ixbug.text), case.sstatus.text, case.stitle.text))
+        return cls(cases)
+
+
+def get_prompt():
+    return ui.cyan('%s>>> ' % (
+        '[%s] ' % CURRENT_CASE.id if CURRENT_CASE else ''))
+
+
+@command('logon', 'login')
+def logon():
+    '''Logon to FB API.
+
+    Example:
+    >>> logon
+    '''
+    logger.info('Logging in')
+    username = os.environ.get('FBUSER')
+    if username is None:
+        username = raw_input('Username: ')
+    password = os.environ.get('FBPASS')
+    if password is None:
+        password = getpass.getpass()
+    FB.logon(username, password)
+    return set_current_user(FBPerson.get(username))
+
+
+@command('logoff', 'logout')
+def logoff():
+    '''Logoff from FB API.
+
+    Example:
+    >>> logoff
+    '''
+    logger.info('Logging out')
+    FB.logoff()
+    return set_current_user(None)
+
+
+@command('help', '?')
+def help_(*args):
+    '''Show help.
+
+    Example:
+    >>> help
+    >>> help logon
+    '''
+    if len(args) == 0:
+        print 'Fogbugz CLI Help'
+        print
+        print 'Available commands:'
+        for name, cmd in sorted(COMMANDS.iteritems()):
+            print '{} - {}'.format(name.rjust(12), cmd.desc())
+        print
+        print 'Type "help <cmd>" for more.'
+    else:
+        name = args[0]
+        print COMMANDS[name].help()
+
+
+@command('whoami')
+def whoami():
+    '''Shows the current user.
+
+    Example:
+    >>> whoami
+    '''
+    print CURRENT_USER
+
+
+@command('show')
+def show(ixBug=None):
+    '''Show the current ticket.
+
+    Example:
+    >>> show  # shows the current ticket
+    >>> show 1234  # shows ticket 1234
+    '''
+    if ixBug is None:
+        assert_current()
+        print CURRENT_CASE
+    else:
+        case = FBCase(int(ixBug))
+        print case
+
+
+@command('close')
+def close_current():
+    '''Close the current ticket.'''
+    assert_current()
+    CURRENT_CASE.close()
+
+
+@command('reactivate')
+def reactivate_current():
+    '''Reactivate the current ticket.'''
+    assert_current()
+    CURRENT_CASE.reactivate()
+
+
+@command('resolve')
+def resolve_current():
+    '''Resolve the current ticket.'''
+    assert_current()
+    CURRENT_CASE.resolve()
+
+
+@command('reopen')
+def reopen_current():
+    '''Reopen the current ticket.'''
+    assert_current()
+    CURRENT_CASE.reopen()
+
+
+@command('assign')
+def assign_current(*args):
+    '''Assign the current ticket to person.
+
+    Example:
+    >>> assign lorenzo bolla
+    '''
+    assert_current()
+    person = ' '.join(args)
+    CURRENT_CASE.assign(person)
+
+
+@command('search')
+def search(*args):
+    '''Search for cases.
+
+    Example:
+    >>> search carmax
+    >>> search assignedTo:"Lorenzo Bolla" status:Active
+    '''
+    q = ' '.join(args)
+    rs = FBCaseSearch.search(q)
+    print rs
+
+
+@command('mycases')
+def mycases():
+    '''List the cases of the logged in user.
+
+    Example:
+    >>> mycases
+    '''
+    q = 'assignedto:"{}" status:Active'.format(CURRENT_USER.fullname)
+    search(q)
+
+
+def read_():
+    cmdline = raw_input(get_prompt())
+    if not cmdline:
+        return None, None
+    tokens = cmdline.split()
+    cmd, args = tokens[0], tokens[1:]
+    return cmd, args
+
+
+def assert_current():
+    assert CURRENT_CASE is not None, 'Pick a case first!'
+
+
+def exec_(cmd, args):
+
+    if cmd.isdigit():
+        show(cmd)
+
+    else:
+        f = COMMANDS.get(cmd)
+        assert f is not None, 'Unknown command {}'.format(cmd)
+        return f(*args)
+
+
+@command('quit', 'exit', 'bye')
+def quit_():
+    '''Quit.
+
+    Example:
+    >>> quit
+    '''
+    print 'Bye!'
+    sys.exit(0)
+
+
+def main():
+    ui.init_readline()
+    logon()
+    try:
+        while True:
+            try:
+                cmd, args = read_()
+                if cmd is None:
+                    continue
+                exec_(cmd, args)
+            except EOFError:
+                quit_()
+            except KeyboardInterrupt:
+                pass
+            except Exception:
+                logger.exception('ERROR')
+    finally:
+        logoff()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ui.py
@@ -0,0 +1,120 @@
+from functools import partial, wraps
+import atexit
+import fcntl
+import os
+import readline
+import struct
+import sys
+import termios
+
+
+def colorize(color, s):
+    if _supports_color(sys.stdout):
+        color_open = '\033[{}m'.format(color)
+        color_close = '\033[0m'
+    else:
+        color_open = color_close = ''
+    if not isinstance(s, basestring):
+        s = unicode(s)
+    return color_open + s.encode('utf-8') + color_close
+
+
+def _supports_color(stream):
+
+    try:
+        import curses
+    except ImportError:
+        curses = None
+
+    color = False
+    if curses and hasattr(stream, 'isatty') and stream.isatty():
+        try:
+            curses.setupterm()
+            if curses.tigetnum("colors") > 0:
+                color = True
+        except Exception:
+            pass
+    return color
+
+
+# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+black = partial(colorize, '0;30')
+red = partial(colorize, '0;31')
+green = partial(colorize, '0;32')
+yellow = partial(colorize, '0;33')
+blue = partial(colorize, '0;34')
+purple = partial(colorize, '0;35')
+cyan = partial(colorize, '0;36')
+gray = partial(colorize, '0;37')
+darkgray = partial(colorize, '1;30')
+
+
+def status(s):
+    color_for_status = {
+        'active': green,
+        'testing': yellow,
+        'needs additional': purple,
+        'resolved': gray,
+        'closed': darkgray,
+    }
+    s_ = s.strip().lower()
+
+    for st, color in color_for_status.iteritems():
+        if st in s_:
+            return color(s)
+    return s
+
+
+def _get_hw():
+    try:
+        return struct.unpack(
+            'hh', fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))
+    except Exception:
+        return (25, 80)
+
+
+HEIGHT, WIDTH = _get_hw()
+hl1 = yellow('=' * WIDTH)
+hl2 = yellow('-' * WIDTH)
+
+
+def rtrunc(s, n):
+    return s[:n].rjust(n)
+
+
+def ltrunc(s, n):
+    return s[:n].ljust(n)
+
+
+def completer(text, state):
+    from fbcli import COMMANDS
+
+    all_options = COMMANDS.keys()
+    # TODO persons
+    # TODO case ids
+
+    options = [x for x in all_options if x.startswith(text)]
+    try:
+        return options[state]
+    except IndexError:
+        return None
+
+
+def ignoring_IOerror(f):
+    @wraps(f)
+    def helper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except IOError:
+            pass
+    return helper
+
+
+def init_readline():
+    readline.set_completer(completer)
+    readline.set_completer_delims(' \t\n')
+    readline.parse_and_bind('tab: complete')
+    histfile = os.path.join(os.path.expanduser("~"), ".fbcli_history")
+    ignoring_IOerror(readline.read_history_file)(histfile)
+    atexit.register(ignoring_IOerror(readline.write_history_file), histfile)
+    del histfile
