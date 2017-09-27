6c9301908db9ca2df67e39a2c61b03217e868f11
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2017-09-27 11:54:20 +0100

    Fix tests

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,4 +5,4 @@ from fbcli import ui
 class TestReadline(unittest.TestCase):
 
     def test_completer(self):
-        self.assertEqual(ui.completer('mycase', 0), 'mycases')
+        self.assertEqual(ui.completer('mycase', 0), 'mycases ')
